7c8c09daa810fddd7601ed58cf8205ea1991db1c
Alex Oliver <alex@bsns-book.local>
2023-09-21 14:54:06 -0500

    Add Hello World server to main

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/hello", getHello)
+
+	err := http.ListenAndServe(":8080", nil)
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+	}
+}
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		fmt.Printf("got an invalid request")
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Printf("got / request\n")
+	io.WriteString(w, "Root\n")
+}
+
+func getHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got /hello request\n")
+	io.WriteString(w, "Hello, World!\n")
+}
